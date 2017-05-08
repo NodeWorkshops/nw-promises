f10fded5b85c669053ff824749028cd7decf8513
Fabio Ponciroli <ponch78@gmail.com>
2017-05-08 21:10:03 +0100

    Added note

--- read-file.js
+++ read-file.js
@@ -6,6 +6,7 @@ const result = fs.readFileSync('./data2.txt');
 console.log("Done reading file");
 
 // Show that it DOESN'T block before printing "Done reading file"
+// Introduce concept of callback
 fs.readFile('./data2.txt', (err, data) => {
   if (err) throw err;
   console.log("Done reading async!");
