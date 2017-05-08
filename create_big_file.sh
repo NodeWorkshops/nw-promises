ea59d412e22c03ce880c1a135fb4201f9ba054c9
Fabio Ponciroli <ponch78@gmail.com>
2017-05-08 20:59:38 +0100

    Reordere examples

---
+++ create_big_file.sh
@@ -0,0 +1,1 @@
+yes "Some text" | head -n 100000000 > data2.txt
--- read-file.js
+++ read-file.js
@@ -1,11 +1,12 @@
-
 const fs = require("fs");
 
-fs.readFile('./data1.txt', (err, data) => {
-  if (err) throw err;
-  console.log("readFile: ", data.toString());
-});
-
-
+// Show that it blocks before printing "Done reading file"
+console.log("Start to read file");
 const result = fs.readFileSync('./data2.txt');
-console.log("readFileSync: ", result.toString());
+console.log("Done reading file");
+
+// Show that it DOESN'T block before printing "Done reading file"
+fs.readFile('./data2.txt', (err, data) => {
+  if (err) throw err;
+  console.log("Done reading async!");
+});
