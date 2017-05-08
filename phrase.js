39c954f5f208d4ea1ab9ebb878756f3e4ef930f2
Daryn Holmes <darynholmes@gmail.com>
2017-05-08 22:50:06 +0100

    phrase with promise

--- phrase.js
+++ phrase.js
@@ -1,11 +1,15 @@
 const request = require("request");
 
-
-request('http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en&key=23', function (error, response, body) {
-  if (error || response.statusCode != 200) {
-    console.log("There was an error fetching the quote");
-  } else {
-    const {quoteText} = JSON.parse(body.replace("\\'", "'"));
-    console.log("Quote:", quoteText);
-  }
-});
+function fetchPhrase(cb) {
+    const key = Math.floor(Math.random() * 10) + 1
+    request(`http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en&key=${key}`, function (error, response, body) {
+      if (error || response.statusCode != 200) {
+        console.log("There was an error fetching the quote");
+        cb('Ops...something went wrong!');
+      } else {
+        const {quoteText} = JSON.parse(body.replace(/\'/g, "'"));
+        // console.log("Quote:", quoteText);
+        cb(null, quoteText);
+      }
+    });
+}
